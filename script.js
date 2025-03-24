da88a52074cef4c36847eac1b796105e01e4c381
Somanath Shahaji kakade <somanath.kakade5@gmail.com>
2025-03-24 10:50:13 +0530

    added space

--- script.js
+++ script.js
@@ -12,9 +12,12 @@ function colorChange() {
   const red = Math.floor(Math.random() * 255);
   const green = Math.floor(Math.random() * 255);
   const blue = Math.floor(Math.random() * 255);
+
   colorCode.textContent = `rgb(${red},${green},${blue})`;
+
   document.body.style.backgroundColor = `rgb(${red},${green},${blue})`;
 
+  localStorage.setItem('colorValue',)
 }
 
 
